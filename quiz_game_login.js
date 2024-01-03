9e5e7980eb6a82b1408b6abbab47233cd369fd6b
AbhiramKakarla5679 <154550692+AbhiramKakarla5679@users.noreply.github.com>
2024-01-03 17:44:08 +0000

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,12 +1,9 @@
-var canvas = new fabric.Canvas('myCanvas');
- var x= document.getElementById("myAudio");
-
-function new_image()
-{
-	
-	
-}
-
-function playSound(){
-	
-}
+function addUser() {
+    player1_name = document.getElementById("Player1_name_input").value;
+    player2_name = document.getElementById("Player2_name_input").value;
+    
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+    
+    window.location = "quiz_game_page.html";
+    }
